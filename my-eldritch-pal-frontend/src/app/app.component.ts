e5dc4dde3ac056bf162dba1ac3aed39891990067
Alexandre Mendes <alexandremendesprofissional@gmail.com>
2024-10-29 18:34:20 +0000

    Initial website setup. Added toolbar. Homepage setup.

---
+++ my-eldritch-pal-frontend/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { HomeComponent } from "./home/home.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MatSidenavModule, MatButtonModule, MatIconModule,
+    HeaderComponent, HomeComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements AfterViewInit{
+  @ViewChild('header', { static: true, read:ElementRef }) header!: ElementRef;
+  headerHeight: number = 0;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+  
+  ngAfterViewInit() {
+    this.headerHeight = this.header.nativeElement.offsetHeight;
+    this.cdr.detectChanges();
+  }
+}
---
+++ my-eldritch-pal-frontend/src/app/header/header.component.ts
@@ -0,0 +1,47 @@
+import { Component, HostListener, Input } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenav, MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatToolbarModule,MatIconModule,MatSidenavModule,MatButtonModule, CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  @Input() sidenav!: MatSidenav;
+
+  constructor() {}
+
+  toggleSidenav() {
+    this.sidenav.toggle();
+  }
+
+  backgroundColor = 'black';
+  textColor = 'white';
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollOffset = 300; // The scroll position when the toolbar should be fully white
+    const scrollPosition = window.scrollY;
+
+    const progress = Math.min(scrollPosition / scrollOffset, 1);
+
+    // Background
+    const r = Math.round(255 * progress); 
+    const g = Math.round(255 * progress); 
+    const b = Math.round(255 * progress); 
+    this.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+
+    // Text
+    const rText = Math.round(255 * (1 - progress));
+    const gText = Math.round(255 * (1 - progress));
+    const bText = Math.round(255 * (1 - progress));
+    this.textColor = `rgb(${rText}, ${gText}, ${bText})`;
+  }
+}
